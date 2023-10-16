a6da19ddd925f41c2aca9a6c039513df3608db6c
Kamil Drozdz <kamil12x@wp.pl>
2023-10-16 08:16:40 +0200

    hotfix: changing prettier, adding another option to typescript when there is an error that prevents building the project and passing git acitons

--- src/components/pages/User/List/content/ListContent.tsx
+++ src/components/pages/User/List/content/ListContent.tsx
@@ -3,14 +3,14 @@ import UserList from './UserList';
 import { useState } from 'react';
 
 const ListContent = () => {
-	const [filters, setFilters] = useState({ role: 'All', plan: 'All', status: 'All' });
+  const [filters, setFilters] = useState({ role: 'All', plan: 'All', status: 'All' });
 
-	return (
-		<div className='space-y-6'>
-			<UserFilter filters={filters} setFilters={setFilters} />
-			<UserList filters={filters} />
-		</div>
-	);
+  return (
+    <div className="space-y-6">
+      <UserFilter filters={filters} setFilters={setFilters} />
+      <UserList filters={filters} />
+    </div>
+  );
 };
 
 export default ListContent;
--- src/components/pages/User/List/content/UserFilter.tsx
+++ src/components/pages/User/List/content/UserFilter.tsx
@@ -5,79 +5,79 @@ export const roles = ['All', 'Author', 'Editor', 'Admin', 'Subscriber'];
 export const plans = ['All', 'Basic', 'Company', 'Enterprise', 'Team'];
 export const statuses = ['All', 'Pending', 'Inactive', 'Active'];
 const UserFilter = ({ filters, setFilters }) => {
-	return (
-		<CardContainer>
-			<p>Search Filter</p>
-			<div className='flex space-x-2'>
-				<Select onValueChange={e => setFilters({ ...filters, role: e as string })}>
-					<SelectTrigger className='md:w-[150px]  whitespace-nowrap mx-2'>
-						<SelectValue
-							placeholder={
-								<div className='flex justify-center items-center space-x-2'>
-									<p>All</p>
-								</div>
-							}
-						/>
-					</SelectTrigger>
-					<SelectContent className='border-darkBlue'>
-						<SelectGroup className='dark:bg-mediumBlue bg-lightWhite dark:text-gray-200'>
-							{roles.map(role => (
-								<SelectItem value={role} key={role}>
-									<div className='flex justify-center items-center space-x-2'>
-										<p>{role}</p>
-									</div>
-								</SelectItem>
-							))}
-						</SelectGroup>
-					</SelectContent>
-				</Select>
-				<Select onValueChange={e => setFilters({ ...filters, plan: e as string })}>
-					<SelectTrigger className='md:w-[150px]  whitespace-nowrap mx-2'>
-						<SelectValue
-							placeholder={
-								<div className='flex justify-center items-center space-x-2'>
-									<p>All</p>
-								</div>
-							}
-						/>
-					</SelectTrigger>
-					<SelectContent className='border-darkBlue'>
-						<SelectGroup className='dark:bg-mediumBlue bg-lightWhite dark:text-gray-200'>
-							{plans.map(plan => (
-								<SelectItem value={plan} key={plan}>
-									<div className='flex justify-center items-center space-x-2'>
-										<p>{plan}</p>
-									</div>
-								</SelectItem>
-							))}
-						</SelectGroup>
-					</SelectContent>
-				</Select>
-				<Select onValueChange={e => setFilters({ ...filters, status: e as string })}>
-					<SelectTrigger className='md:w-[150px]  whitespace-nowrap mx-2'>
-						<SelectValue
-							placeholder={
-								<div className='flex justify-center items-center space-x-2'>
-									<p>All</p>
-								</div>
-							}
-						/>
-					</SelectTrigger>
-					<SelectContent className='border-darkBlue'>
-						<SelectGroup className='dark:bg-mediumBlue bg-lightWhite dark:text-gray-200'>
-							{statuses.map(status => (
-								<SelectItem value={status} key={status}>
-									<div className='flex justify-center items-center space-x-2'>
-										<p>{status}</p>
-									</div>
-								</SelectItem>
-							))}
-						</SelectGroup>
-					</SelectContent>
-				</Select>
-			</div>
-		</CardContainer>
-	);
+  return (
+    <CardContainer>
+      <p>Search Filter</p>
+      <div className='flex space-x-2'>
+        <Select onValueChange={(e) => setFilters({ ...filters, role: e as string })}>
+          <SelectTrigger className='md:w-[150px]  whitespace-nowrap mx-2'>
+            <SelectValue
+              placeholder={
+                <div className='flex justify-center items-center space-x-2'>
+                  <p>All</p>
+                </div>
+              }
+            />
+          </SelectTrigger>
+          <SelectContent className='border-darkBlue'>
+            <SelectGroup className='dark:bg-mediumBlue bg-lightWhite dark:text-gray-200'>
+              {roles.map((role) => (
+                <SelectItem value={role} key={role}>
+                  <div className='flex justify-center items-center space-x-2'>
+                    <p>{role}</p>
+                  </div>
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Select onValueChange={(e) => setFilters({ ...filters, plan: e as string })}>
+          <SelectTrigger className='md:w-[150px]  whitespace-nowrap mx-2'>
+            <SelectValue
+              placeholder={
+                <div className='flex justify-center items-center space-x-2'>
+                  <p>All</p>
+                </div>
+              }
+            />
+          </SelectTrigger>
+          <SelectContent className='border-darkBlue'>
+            <SelectGroup className='dark:bg-mediumBlue bg-lightWhite dark:text-gray-200'>
+              {plans.map((plan) => (
+                <SelectItem value={plan} key={plan}>
+                  <div className='flex justify-center items-center space-x-2'>
+                    <p>{plan}</p>
+                  </div>
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Select onValueChange={(e) => setFilters({ ...filters, status: e as string })}>
+          <SelectTrigger className='md:w-[150px]  whitespace-nowrap mx-2'>
+            <SelectValue
+              placeholder={
+                <div className='flex justify-center items-center space-x-2'>
+                  <p>All</p>
+                </div>
+              }
+            />
+          </SelectTrigger>
+          <SelectContent className='border-darkBlue'>
+            <SelectGroup className='dark:bg-mediumBlue bg-lightWhite dark:text-gray-200'>
+              {statuses.map((status) => (
+                <SelectItem value={status} key={status}>
+                  <div className='flex justify-center items-center space-x-2'>
+                    <p>{status}</p>
+                  </div>
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+    </CardContainer>
+  );
 };
 
 export default UserFilter;
