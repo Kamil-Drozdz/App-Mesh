79c99172b3d5a691251bf9eb491a175547d085c2
Kamil Dróżdż <kamil12x@wp.pl>
2023-10-23 21:54:55 +0200

    build: using bundling analysis and dividing files into smaller chunks to increase optimization.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { PluginOption, defineConfig } from 'vite';
 import viteCompression from 'vite-plugin-compression';
 import Svgr from 'vite-plugin-svgr';
 
@@ -8,6 +9,12 @@ export default defineConfig({
   plugins: [
     Svgr(),
     react(),
+    visualizer({
+      template: 'treemap',
+      gzipSize: true,
+      brotliSize: true,
+    }) as PluginOption,
+
     viteCompression({
       verbose: true,
       disable: false,
@@ -16,8 +23,52 @@ export default defineConfig({
     }),
   ],
   build: {
-    sourcemap: true,
     minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-chunk': ['react', 'react-dom', 'react-router-dom'],
+          'fullcalendar-chunk': [
+            '@fullcalendar/core',
+            '@fullcalendar/daygrid',
+            '@fullcalendar/interaction',
+            '@fullcalendar/react',
+          ],
+          'radixui-chunk': [
+            '@radix-ui/react-popover',
+            '@radix-ui/react-radio-group',
+            '@radix-ui/react-select',
+            '@radix-ui/react-separator',
+            '@radix-ui/react-slot',
+            '@radix-ui/react-tooltip',
+          ],
+          'data-fetching-chunk': ['axios', 'date-fns'],
+          'ui-chunk': [
+            'react-beautiful-dnd',
+            'react-loader-spinner',
+            'react-toastify',
+            'react-transition-group',
+            'chart.js',
+            'react-icons',
+            'tailwindcss-animate',
+          ],
+          'utility-chunk': [
+            'lodash',
+            'uuid',
+            'zustand',
+            'zod',
+            'firebase/app',
+            'firebase/analytics',
+            'firebase/auth',
+            'firebase/firestore',
+            'firebase/performance',
+            'i18next',
+            'jspdf',
+          ],
+          'faker-chunk': ['faker', '@faker-js/faker'],
+        },
+      },
+    },
   },
   resolve: {
     alias: {
